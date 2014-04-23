9f2f57738e6acc6138c49614b42a34107cfc6c10
Hendrix2005 <Hendrix2005@Zombo.com>
2014-04-23 16:17:13 -0700

    dont forget, Comm-C to copy lerned a .cool thing

--- justice.py
+++ justice.py
@@ -1,1 +1,15 @@
 # author: Hendrix2005
+
+import turtle
+hendrix = turtle.Turtle()
+sides = 5
+angle = 150 - (180 * (sides - 2) / sides)
+length = 100
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+
+for number in numbers:
+	hendrix.forward(100)
+	hendrix.left(angle)
+
+
