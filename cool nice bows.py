805ff6e1a11bf36f887791368502bc7e4b409f7d
Hendrix2005 <Hendrix2005@Zombo.com>
2014-04-23 17:12:10 -0700

    pythonroom update

--- cool nice bows.py
+++ cool nice bows.py
@@ -1,1 +1,14 @@
 # author: Hendrix2005
+import turtle
+t = turtle.Turtle()
+a = "true"
+number = 1
+while a == "true":
+	a = "false"
+	t.forward(number)
+	t.left(90)
+while a == "false":
+	a = "true"
+	number + 1
+	
+	
