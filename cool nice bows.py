54249e014f5ae7cf9aac023eefbbff7e2813fed1
Hendrix2005 <Hendrix2005@Zombo.com>
2014-04-23 17:17:13 -0700

    pythonroom update

--- cool nice bows.py
+++ cool nice bows.py
@@ -1,14 +1,14 @@
 # author: Hendrix2005
 import turtle
 t = turtle.Turtle()
-a = "true"
-number = 1
-while a == "true":
-	a = "false"
-	t.forward(number)
+a = 1
+
+numbers = range(0, 100)
+for number in numbers:
+	t.forward(a)
+	a = a + 1
 	t.left(90)
-while a == "false":
-	a = "true"
-	number + 1
+	
+
 	
 	
